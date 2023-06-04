3ba65df47c49bbe662ff73e1014b319503e01bb8
ASekretarski <artur.sekretarski@gmail.com>
2023-06-05 01:09:21 +0200

    hw 11 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const API_PATH = "https://pixabay.com/api/"
 const API_KEY = "37026170-4b9a19924d092957be3a6c20c";
@@ -8,26 +10,96 @@ const searchParameters = new URLSearchParams({
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 40,
+    page: 1,
 })
+const searchInput = document.querySelector('[name="searchQuery"]')
+const searchForm = document.querySelector('#search-form')
+const gallery = document.querySelector('.gallery')
+const loadButton = document.querySelector('.load-more')
 let searchPhrase = '';
 const noMatches = "Sorry, there are no images matching your search query. Please try again."
+let page = 1;
 
-// const axiosResult = await axios.get(`${API_PATH}?${searchParameters}&q=${searchPhrase}`)
-
-async function imageSearch(searchPhrase) {
+async function imageSearch(searchPhrase, page=1) {
     try {
-        const response = await axios.get(`${API_PATH}?${searchParameters}&q=${searchPhrase}`);
-        console.log(response)
+        const response = await axios.get(`${API_PATH}?${searchParameters}&q=${searchPhrase}&page=${page}`);
         const { data } = response;
-        console.log(data.total)
+        if (page === 1) {
+        if (!searchPhrase) {
+            return(data)
+            }
         if (data.total === 0) {
             Notiflix.Notify.warning(`${noMatches}`);
-        } else {
+            } else {
             Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+            }            
         }
-    } catch(e) {
-        Notiflix.Notify.failure(e.response.data)
+        return (data)
+    } catch (e) {
+        Notiflix.Notify.failure(`Error status ${e.response.status}`)
     }
 }
-
-imageSearch('cat with a dog')
+function createGallery(data) {
+    const galleryData = data.hits.map(
+        ({comments, downloads, largeImageURL, likes, tags, views, webformatURL})=>{return `<div class="photo-card">
+        <a class="gallery__item" href="${largeImageURL}">
+        <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+        <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+        </p>
+        <p class="info-item">
+        <b>Views</b>
+        ${views}
+        </p>
+        <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+        </p>
+        <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+        </p>
+        </div>
+        </div>`}
+    ).join('')
+    gallery.insertAdjacentHTML('beforeend', galleryData);
+    const allGalleryLinks = document.querySelectorAll('.gallery__item')
+    for (const link of allGalleryLinks) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault()
+        }) 
+    }
+    var lightbox = new SimpleLightbox('.gallery a');
+}
+async function showGallery(searchPhrase) {
+    const result = await imageSearch(searchPhrase)
+    createGallery(result)
+    if (result.totalHits > result.hits.length) {
+        loadButton.classList.remove('invisible')
+    } else {
+        loadButton.classList.add('invisible')
+    }
+}
+async function loadMoreImages() {
+    page += 1;
+    const result = await imageSearch(searchPhrase, page)
+    createGallery(result)
+    if (page===Math.ceil(result.totalHits/40)){
+        loadButton.classList.add('invisible');
+        Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`)
+    }
+}
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    gallery.innerHTML = '';
+    page = 1;
+    searchPhrase=searchInput.value
+    showGallery(searchPhrase)
+})
+loadButton.addEventListener("click", () => {
+    loadMoreImages()
+})
